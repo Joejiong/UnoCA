4b0fc127f6bec286aa758f3054c16aaa41446316
Joejiong <1004691415@qq.com>
2017-08-10 17:46:11 +0800

    haha

--- src/app/components/header.component.ts
+++ src/app/components/header.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
+import {Card}from '../model/card';
+
 
 @Component({
   selector: 'app-header',
@@ -7,7 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  public value:string= "0";
+
+    color:string = "red";
+    
+    image:string = '../assets/uno_deck';
   constructor() { }
+
+
+  @Input()card:Card[];
 
   ngOnInit() {
   }
--- src/app/model/card.ts
+++ src/app/model/card.ts
@@ -1,8 +1,8 @@
 export interface Card{
 
-    value:string;
+    value:number;
     color:string;
-    image:string;
+    imageUrl:string;
 
 };
 
@@ -11,4 +11,46 @@ export interface BasketItem{
     items: Card[];
 
     player:string;
+}
+
+export class UnoDeck{
+
+    private cards :Card[]=[];
+    
+
+    private static COLOURS: string[] =  ["red","green", "blue", "yellow"]
+    //private static CARD_NAME =
+
+    constructor(){
+
+            let prefix:string ="0";
+
+        //creat 
+        for(let i ;  i<2;i++){
+
+            for(let j ;j<12; j++){
+
+                let prefix = "0";
+                if (j<10) {
+                    prefix = '0';
+                }else{
+
+
+                    prefix = j;
+
+                }
+                //this.cards.push({value:j+1,color:})
+
+
+            }
+
+        }
+
+
+        //
+
+    }
+
+    //
+
 }
