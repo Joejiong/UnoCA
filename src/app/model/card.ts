b45b592f6f47a824d8e6beae6535c8d819436cd1
Joejiong <1004691415@qq.com>
2017-08-10 18:26:29 +0800

    hahahhahah

--- src/app/model/card.ts
+++ src/app/model/card.ts
@@ -21,6 +21,16 @@ export class UnoDeck{
     private static COLOURS: string[] =  ["red","green", "blue", "yellow"]
     //private static CARD_NAME =
 
+    private shuffle(times: number = 5):void{
+        //google, polynomial time, or number times o to power of n & n = array(big o notation); search times ia o(ln2n)
+    }
+    
+    public take(): Card{
+        
+        return (this.cards.pop());
+    }
+  
+
     constructor(){
 
             let prefix:string ="0";
@@ -28,18 +38,27 @@ export class UnoDeck{
         //creat 
         for(let i ;  i<2;i++){
 
-            for(let j ;j<12; j++){
+            for(var c = 0;UnoDeck.COLOURS.length;c++)
+                for(let j ;j<12; j++){
 
                 let prefix = "0";
                 if (j<10) {
-                    prefix = '0';
+
+                            
+                    prefix = prefix + j;
+
                 }else{
 
 
-                    prefix = j;
+
+                    prefix = j + "";
 
                 }
-                //this.cards.push({value:j+1,color:})
+                    this.cards.push({
+                    value: j+1, 
+                    color:UnoDeck.COLOURS[c],
+                    imageUrl:"/assets/uno_deck/c" +c + "_" + prefix +".png"
+                })
 
 
             }
